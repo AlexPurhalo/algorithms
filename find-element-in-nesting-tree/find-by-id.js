3c4ac2f7a157d9d51cb00e8292dfd4699babc553
AlexPurhalo <alexpurhalo@gmail.com>
2017-08-04 19:54:12 +0300

    fixed-algorithm

--- find-element-in-nesting-tree/find-by-id.js
+++ find-element-in-nesting-tree/find-by-id.js
@@ -1,98 +1,4 @@
-// Data
-const allTunnels = [
-	{
-		id: 29
-	},
-	{
-		id: 211,
-	},
-	{
-		id: 1,
-		diamond: false,
-		tunnels: [
-			{
-				id: 2,
-				diamond: true,
-				tunnels: [
-					{
-						id: 3,
-						diamond: false,
-						tunnels: [
-							{
-								id: 4,
-								diamond: false,
-								tunnels: [
-									{
-										id: 5,
-										diamond: false
-									},
-									{
-										id: 6,
-										diamond: true
-									},
-									{
-										id: 7,
-										diamond: false,
-									}
-								]
-							},
-							{
-								id: 8,
-								diamond: false
-							},
-							{
-								id: 9,
-								diamond: false,
-								tunnels: [
-									{
-										id: 10,
-										diamond: false,
-									}
-								]
-							}
-						]
-					},
-					{
-						id: 11,
-						diamond: false
-					}
-				]
-			},
-			{
-				id: 12,
-				diamond: true,
-				tunnels: [
-					{
-						id: 13,
-						diamond: false
-					}
-				]
-			},
-			{
-				id: 14,
-				diamond: false
-			}
-		]
-	},
-	{
-		id: 15,
-		diamond: false,
-		tunnels: [
-			{
-				id: 16,
-				diamond: false,
-				tunnels: [
-					{
-						id: 17
-					}
-				]
-			}
-		]
-	}
-]
-
-const findITemById = (initItems, id, items) => {
-	// Data
+const findItemById = (initItems, id, items) => {
 	const parentItems = []
 	
 	// Actions
@@ -100,85 +6,87 @@ const findITemById = (initItems, id, items) => {
 	const goBack   = (p) => p.slice(0, p.length-1)
 	const goToNext = (p) => p.map((item, i) => i === p.length-1 ? item+1 : item)
 	
-	// Conditions
-	const isExactItem  = (item, currId) => item['id'] === currId
-	const hasItems     = (item) => item[items]
-	const isItemExist  = (parItem, p) => parItem[items][p[p.length-1]]
-	const isOverSearch = (p) => p[p.length-1] >= initItems.length
+	// Condition
+	const isExactItem     = (item, currId) => item['id'] === currId
+	const hasSubItems     = (item)         => item[items]
+	const isSubItemExist  = (parItem, p)   => parItem[items][goDown(p)[p.length]]
+	const hasNextSubItem  = (parItem, p)   => parItem[items][goToNext(p)[p.length-1]]
+	const areParentsExist = ()             => parentItems.length > 0
+	const hasNextItem     = (p)            => initItems[goToNext(p)[p.length-1]]
+	const isItemExist     = (items, p)     => items[goToNext(goBack(p))[p.length-2]]
 	
-	// Function
-	const findItem = (currItems, currId, position) => {
-		// console.log(currItems)
-		console.log(`init: [${position}]`)
-		
+	// Main logic
+	const find = (currItems, currId, position) => {
 		const currItem = currItems[position[position.length-1]]
-		
 		
 		if (isExactItem(currItem, currId)) return currItem
 		
-		position = goDown(position)
-		
-		if (hasItems(currItem) && isItemExist(currItem, position)) {
+		if (hasSubItems(currItem) && isSubItemExist(currItem, position)) {
 			parentItems.unshift(currItem)
-			return findItem(currItem[items], currId, position)
+			
+			return find(currItem[items], currId, goDown(position))
 		}
 		
-		// TODO: change to conditions
+		if (areParentsExist() && hasNextSubItem(parentItems[0], position))
+			return find(parentItems[0][items], currId, goToNext(position))
 		
-		if (parentItems.length === 0) {
-			position = goBack(position)
-			position = goToNext(position)
+		if (areParentsExist()) {
+			const goToNextParentsItem = (p) => {
+				parentItems.splice(0, 1)
+				
+				if(!areParentsExist() && isItemExist(initItems, p))
+					return find(initItems, currId, goToNext(goBack(p)))
+				
+				if (areParentsExist()) {
+					if (isItemExist(parentItems[0][items], p))
+						return find(parentItems[0][items], currId, goToNext(goBack(p)))
+					
+					return goToNextParentsItem(goToNext(goBack(p)))
+				}
+				
+				return -1
+			}
 			
-			if (position >= initItems.length-1) console.log('mm')
-			
-			if (isOverSearch(position)) return -1
-			
-			return findItem(initItems, currId, position)
+			return goToNextParentsItem(position)
 		}
 		
-		
-		
-		// TODO: change to conditions
-		if (parentItems.length === 1) {
-			
-			parentItems.splice(0, parentItems.length)
-			position = goBack(position)
-			position = goBack(position)
-			position = goToNext(position)
-			
-			// console.log(`currP: ${position[position.length-1]}, max: ${initItems.length-1}`)
-			if (isOverSearch(position)) return -1
-			
-			const nextItem = initItems[position[position.length-1]]
-			
-			if (isExactItem(nextItem, currId)) return nextItem
-			
-			position = goDown(position)
-			
-			console.log(position)
-			
-			return findItem(nextItem[items], currId, position)
-		}
-		
-		const findNextItem = (p) => {
-			const parItem = parentItems[0]
-			
-			p = goBack(p)
-			p = goToNext(p)
-			
-			if (isItemExist(parItem, p))
-				return findItem(parItem[items], currId, p)
-			
-			parentItems.shift()
-			
-			return findNextItem(p)
-		}
-		
-		return findNextItem(position)
+		if (hasNextItem(position))
+			return find(initItems, currId, goToNext(position))
 	}
 	
-	return findItem(initItems, id, [0])
+	return find(initItems, id, [0])
 }
 
-// console.log(findITemById(allTunnels, 16, 'tunnels'))
-console.log(findITemById(allTunnels, 18, 'tunnels'))
+const initCategories = [
+	{
+		id: 1,
+		categories: [
+			{ id: 3,
+				categories: [
+					{
+						id: 5,
+						categories: [{ id: 6 }]
+					}
+				]
+			},
+			{ id: 4 },
+			{ id: 7 },
+			{ id: 8, categories: [{ id: 9 }] },
+			{ id: 10 }
+		]
+	},
+	{
+		id: 2
+	},
+	{
+		id: 3,
+		categories: [
+			{
+				id: 99,
+				categories: [ { id: 100 } ]
+			}
+		]
+	}
+]
+
+console.log(findItemById(initCategories, 101, 'categories'))
